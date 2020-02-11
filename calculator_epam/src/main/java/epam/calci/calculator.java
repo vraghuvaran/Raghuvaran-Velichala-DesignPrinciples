baa88b6186ae2aaed4ea38e41767977b865b459c
vraghuvaran <vraghuvaran123@gmail.com>
2020-02-11 15:22:30 +0530

    initial commit

---
+++ calculator_epam/src/main/java/epam/calci/calculator.java
@@ -0,0 +1,25 @@
+package epam.calci;
+
+public class calculator {
+	int a;
+	int b;
+	int ans;
+	public calculator() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public int add(int a, int b) {
+		return (a+b);
+	}
+	
+	public int sub(int a, int b) {
+		return (a+b);
+	}
+	
+	public int mul(int a, int b) {
+		return (a+b);
+	}
+	public int div(int a, int b) {
+		return (a+b);
+	}
+}
---
+++ calculator_epam/src/main/java/epam/calculator_epam/App.java
@@ -0,0 +1,16 @@
+package epam.calculator_epam;
+
+import epam.frontend.Window;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Window window = new Window();
+        window.setVisible(true);
+    }
+}
