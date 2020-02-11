7830c3556ef861661df2da8e0da8f512411fe7e0
vraghuvaran <40687346+vraghuvaran@users.noreply.github.com>
2020-02-11 22:27:39 +0530

    Update calculator.java

--- calculator_epam/src/main/java/epam/calci/calculator.java
+++ calculator_epam/src/main/java/epam/calci/calculator.java
@@ -13,13 +13,13 @@ public class calculator {
 	}
 	
 	public int sub(int a, int b) {
-		return (a+b);
+		return (a-b);
 	}
 	
 	public int mul(int a, int b) {
-		return (a+b);
+		return (a*b);
 	}
 	public int div(int a, int b) {
-		return (a+b);
+		return (a/b);
 	}
 }
